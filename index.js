e3688967fd78ac0ed38e5e8ac76d1fd09b8f9702
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-03 15:16:17 -0500

    creating routes for podcast and frequency controllers

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-Hello world
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.koin(__dirname, 'public')));
+
+
+
+app.get('/', frequencies.index);
+
+
+app.use('/frequencies', require('./controllers/frequencies'));
+app.use('/podcasts', require('./controllers/podcasts'));
+
+
+
+app.listen(4000, function(){
+  console.log("We are up and running on port 4000!");
+});
