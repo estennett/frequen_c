2027c86a5c1864662a66b97df4d40bf1f115ea5f
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-05 18:01:58 -0500

    all vars declared in index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var express = require('express');
-var mongoose = require('mongoose');
-
-//this allowed us to get data from db in json format for our routes, why did it work?
-//we will have a headache when we deploy to heroku
-mongoose.connect('mongodb://localhost/frequency');
-
 var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
 var hbs = require('hbs');
 var path = require('path');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+mongoose.connect('mongodb://localhost/frequency');//we will have a headache when we deploy to heroku
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use('/', require('./controllers/frequencies'));
+
 
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));//need public directory
 
-app.use('/', require('./controllers/frequencies'));
 
 app.get('/', function(req, res){
   res.render('index.html');
