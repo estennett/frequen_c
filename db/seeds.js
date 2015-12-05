fafef8718244842cbe9a8fbdfac6bd967ef7dc34
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-05 16:09:32 -0500

    more seed data to play with

--- db/seeds.js
+++ db/seeds.js
@@ -25,14 +25,23 @@ var dance = new FrequencyModel({title: "All About Dance", genre: "dance"})//new
 
 var podcast1 = new PodcastModel({title: "Sit Amet"})
 var podcast2 = new PodcastModel({title: "Lacus Nullam"})
-var podcast4 = new PodcastModel({title: "Similique Lorem"})
-var podcast3 = new PodcastModel({title: "Ipsum Dolor"})
+var podcast3 = new PodcastModel({title: "Similique Lorem"})
+var podcast4 = new PodcastModel({title: "Babbleset"})
+var podcast5 = new PodcastModel({title: "Realbuzz"})
+var podcast6 = new PodcastModel({title: "Brightbean"})
+var podcast7 = new PodcastModel({title: "Skidoo"})
+var podcast8 = new PodcastModel({title: "Edgewire"})
+var podcast9 = new PodcastModel({title: "Kwimbee"})
+var podcast10 = new PodcastModel({title: "Blogtags"})
+var podcast11 = new PodcastModel({title: "Thoughtstorm"})
+var podcast12 = new PodcastModel({title: "Skipfire"})
+
 
 var frequencies = [history, music, crime, comedy, beverage, design, science, dance];
-var podcasts = [podcast1, podcast2, podcast3, podcast4];
+var podcasts = [podcast1, podcast2, podcast3, podcast4, podcast5, podcast6, podcast7, podcast8, podcast9, podcast10, podcast11, podcast12];
 
 for( var i = 0; i < frequencies.length; i++){
-  frequencies[i].podcasts.push(podcasts[i], podcasts[i+1]);
+  frequencies[i].podcasts.push(podcasts[i], podcasts[i+5]);
   frequencies[i].save(function(err){
     if (err){
       console.log(err)
