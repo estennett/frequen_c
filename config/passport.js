22c2cf16478711e406ba89f92303899799e5d767
Elise Kain <elise.kain@gmail.com>
2015-12-07 12:34:27 -0500

    Intial twitter oauth

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 var LocalStrategy = require("passport-local").Strategy
+var TwitterStrategy = require('passport-twitter').Strategy;
 var User = require("../models/user");
+var env = require('../env');
 
 module.exports = function(passport){
     passport.serializeUser(function(user, callback) {
@@ -52,4 +54,36 @@ module.exports = function(passport){
       return callback(null, user);
     })
   }));
+
+
+  passport.use('twitter', new TwitterStrategy({
+      consumerKey: env.consumerKey,
+      consumerSecret: env.consumerSecret,
+      callbackUrl: env.callbackUrl
+    }, function(token, secret, profile, done){
+      process.nextTick(function(){
+        User.findOne({'twitter.id': profile.id}, function(err, user){
+          if(err) return done(err);
+
+          // If the user already exists, just return that user.
+          if(user){
+            return done(null, user);
+          } else {
+            // Otherwise, create a brand new user using information passed from Twitter.
+            var newUser = new User();
+
+            // Here we're saving information passed to us from Twitter.
+            newUser.twitter.id = profile.id;
+            newUser.twitter.token = token;
+            newUser.twitter.username = profile.username;
+            newUser.twitter.displayName = profile.displayName;
+
+            newUser.save(function(err){
+              if(err) throw err;
+              return done(null, newUser);
+            })
+          }
+        })
+      })
+    }))
 }
