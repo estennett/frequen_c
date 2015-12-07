44bedbaad2252c51765e064aeda63ef7c97a9ab4
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-07 14:47:53 -0500

    frequency crud actions set up in view

--- public/js/views/frequency/frequencyView.js
+++ public/js/views/frequency/frequencyView.js
@@ -4,15 +4,27 @@
 var FrequencyView = function(frequency){
   this.frequency = frequency;
   this.$el = $('<div class="frequency"></div>');
-  $(".frequencies").append(this.$el);
   this.render(this.frequency);
+  $(".frequencies").append(this.$el);
+
 }
 
 FrequencyView.prototype = {
   render: function(frequency){
     var self = this;
+    self.$el.html(self.frequencyTemplate(self.frequency));
+    // var html = $("<div>");
+    // html.append("<h3 class = 'frequency_title'>" + frequency.title + "</h3>");
+    // $(".frequencies").append(html);
+  },
+
+
+  frequencyTemplate: function(frequency){
     var html = $("<div>");
-    html.append("<h3 class = 'frequency_title'>" + frequency.title + "</h3>");
-    $(".frequencies").append(html);
+    html.append("<h3>" + frequency.title + "</h3>");
+    html.append("<p>" + frequency.podcasts + "</p>");
+    html.append("<button class = 'create-podcast'> Create </button>");
+    html.append("<button class = 'delete-podcast'> Delete </button>");
+    return(html);
   }
-}
+};
