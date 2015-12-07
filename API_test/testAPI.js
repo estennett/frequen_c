4c352499659c6b598c7746d23b19fe434ab8d1e7
Elise Kain <elise.kain@gmail.com>
2015-12-07 17:30:51 -0500

    Add embedded audio and descriptions

--- API_test/testAPI.js
+++ API_test/testAPI.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // Search iTunes API for podcasts based on genre
   var searchPodcast = function(search){
-    var url = "https://itunes.apple.com/search?term=" + search + "&media=podcast";
+    var url = "https://itunes.apple.com/search?term=" + search + "&media=podcast&limit=10";
     $.ajax({
         url: url,
         dataType: 'JSONP'
@@ -15,33 +15,6 @@ $(document).ready(function() {
       console.log(response);
     })
   }
-
-  // Handle Yahoo Response (callback function)
-  var handleResponse = function(response) {
-      console.log(response);
-      console.log(response.query.results.feed.entry[0].title);
-      var text = '', item;
-      for (var i = 0, k = response.query.results.feed.entry.length; i < k; i++) {
-        item = response.query.results.feed.entry[i];
-        text += '<li><a href="' + item.link[0].href + '">' + item.title + '</a></li>';
-      }
-      $('#episodeDisplay').html(text);
-    }
-
-  // Use Yahoo Script to generate episodes
-  var injectYahooScript = function(feed) {
-    feed = encodeURIComponent(feed);
-    // console.log(feed);
-    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20feednormalizer%20where%20url%3D'" + feed + "%20and%20output%3D'atom_1.0'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=handleResponse"
-    // console.log(url);
-    var scriptElement = document.createElement('script');
-    scriptElement.setAttribute('type', 'text/javascript');
-    console.log(scriptElement);
-    scriptElement.setAttribute('src', url);
-    document.getElementsByTagName('head')[0].appendChild(scriptElement);
-  }
-
-  injectScript("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20feednormalizer%20where%20url%3D'http%3A%2F%2Fthatsnormal.libsyn.com%2Frss'%20and%20output%3D'atom_1.0'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=handleYahooResponse", false);
 
   // Create DOM Elements with podcast search results
   var generateList = function(searchResults){
@@ -63,10 +36,7 @@ $(document).ready(function() {
     $(".podcastEntry").click(function(event){
       var div = $(event.target).closest("div");
       var feedUrl = div.find(".feedUrl").text();
-      var widget = episodeWidget(feedUrl);
-      widget += "<div style='font-size:10px; text-align:center; width:300px;'><a href='http://feed.mikle.com/' target='_blank' style='color:#CCCCCC;'>RSS Feed Widget</a></div>"
-      div.append(widget).then(pickEpisodeButton());
-      // pickEpisodeButton();
+      injectYahooScript(feedUrl, div);
     });
   }
 
--- API_test/testRSS.js
+++ API_test/testRSS.js
@@ -1,8 +1,9 @@
-function injectScript(feed) {
+function injectYahooScript(feed, div) {
   console.log(feed);
   feed = encodeURIComponent(feed);
   console.log(feed);
-  var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20feednormalizer%20where%20url%3D'" + feed + "'%20and%20output%3D'atom_1.0'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=handleYahooResponse"
+  var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20feednormalizer%20where%20url%3D'" + feed + "'%20and%20output%3D'atom_1.0'&limit=10&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=handleYahooResponse"
+  console.log(url);
   var scriptElement = document.createElement('script');
   scriptElement.setAttribute('type', 'text/javascript');
   scriptElement.setAttribute('src', url);
@@ -11,15 +12,13 @@ function injectScript(feed) {
 
 function handleYahooResponse(response) {
   console.log(response);
-  console.log(response.query.results.feed.entry[0].title);
   var text = '', item;
-  for (var i = 0, k = response.query.results.feed.entry.length; i < k; i++) {
+  for (var i = 0, k = 10; i < k; i++) {
     item = response.query.results.feed.entry[i];
-    text += '<li><a href="' + item.link[0].href + '">' + item.title + '</a></li>';
+    text += '<li><a href="' + item.link[0].href + '">' + item.title + '</a>';
+    text += '<p>' + item.summary.content + '</p></li>';
+
+    text += '<audio controls><source src="' + item.link[0].href + '" type="audio/ogg"><source src="horse.mp3" type="audio/mpeg">Your browser does not support the audio tag.</audio>'
   }
   $('#episodeDisplay').html(text);
 }
-
-$(document).ready(function() {
-  injectScript("http://thatsnormal.libsyn.com/rss", false);
-});
