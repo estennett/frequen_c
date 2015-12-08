a8c90681a47f488d8b122f68504f27d00ffd9a82
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-08 16:33:21 -0500

    podcast show adjustment

--- public/js/views/podcast/podcastShow.js
+++ public/js/views/podcast/podcastShow.js
@@ -1,6 +1,6 @@
 var PodcastShow = function(frequency){
   var self = this;
   this.$show = $("<div class= 'showPodcast' style= 'display: none' >Show Podcast</div>")
-  var body = $('body');
-  $(body).append(this.$show);
+  var footer = $('footer');
+  $(footer).before(this.$show);
 }
