ae48ee8c3e5bec7f3b33c286762473a695619a73
Eric Stennett <estennett@gmail.com>
2015-12-08 15:38:48 -0500

    trying to add click event

--- public/js/views/frequency/frequencyView.js
+++ public/js/views/frequency/frequencyView.js
@@ -5,15 +5,40 @@ var FrequencyView = function(frequency){
   this.frequency = frequency;
   var self = this;
   this.$el = $("<div class =  " + frequency.id + "></div>");
-  // this.$elshow = $("<div class = frequency-show></div>");
+  this.$elshow = $("<div class = frequency-show></div>");
   this.render(this.frequency);
   var frequencyShow = $('.show');
   $(".frequencies").append(this.$el);
-  $("div[class = "+this.frequency.id +"]").on("click", function(){
+
+  $("div[class = "+ this.frequency.id +"]").on("click", function(){
     frequencyShow.html(self.frequencyShowTemplate(self.frequency));
     $('.frequencies').hide();
     $('.show').show();
+    $(".5665cae5e8e6e21cf97a89f1").on("click", function(){
+      console.log("HIIIIII")
+    })
   })
+
+  //add click event for podcasts displayed in frequency show
+  var podcast = frequency.podcasts;
+
+  function logIt(){
+    console.log("hi");
+  }
+
+  // $.each( podcast, function( index, pod ){
+    // 5665cae5e8e6e21cf97a89f1
+
+    // $("div[class = "+ pod.id +"]").on("click", function(){
+    // })
+      // console.log(pod.id)
+  // });
+
+
+
+  // $(podcast).forEach(function(){
+  //   console.log("hi");
+  // })
 }
 
 FrequencyView.prototype = {
@@ -21,7 +46,6 @@ FrequencyView.prototype = {
     var self = this;
     self.$el.html(self.frequencyTemplate(self.frequency));
   },
-
 
   frequencyTemplate: function(frequency){
     var html = $("<div>");
@@ -32,17 +56,39 @@ FrequencyView.prototype = {
     return(html);
   },
 
+  // podcastRender: function(podcast){
+  //     console.log(podcast)
+  //     $("div[class = " + podcast + "]").on("click", function(){
+  //       console.log("hi")
+  //     })
+
+    // for ( i = 0; i < podcastShow.length; i++){
+    //   // $("div[class = " + podcastShow[i].id + "]").on("click", function(){
+    //     console.log(podcastShow[i].id);
+    //   // })
+    // }
+  // },
+
   frequencyShowTemplate: function(frequency){
     var podcast = frequency.podcasts;
     var html = $("<div>");
     html.append("<h3>" + frequency.title + "</h3>");
     // html.append("<p>" + podcast.title+ "</p>");
     for (var i = 0; i < podcast.length; i ++ ){
-      html.append("<p>" + podcast[i].title+ "</p>");
+      html.append("<div class =  " + podcast[i].id + ">" + podcast[i].title + "</div>");
+      // html.append("<div>" + podcast[i].title+ "</div>");
+      // this.podcastRender(podcast[i].id);
     };
     html.append("<button class = 'create-podcast'> Create </button>");
     html.append("<button class = 'delete-podcast'> Delete </button>");
     return(html);
+  },
+
+  podcastShowTemplate: function(frequency){
+    var html = $("<div>");
+    html.append("<h3>" + podcast.title + "</h3>");
+    html.append("<h3>" + podcast.current + "</h3>");
+    html.append("<h3>" + podcast.title + "</h3>");
   }
 
 }
