430e4452343d71d13e08a3ba94f62610d1db6cf5
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-12-08 16:31:22 -0500

    appended frequency show page above footer

--- public/js/views/frequency/frequencyShow.js
+++ public/js/views/frequency/frequencyShow.js
@@ -1,9 +1,8 @@
 
 var FrequencyShow = function(frequency){
-  var self = this;
   this.$show = $("<div class= 'show' style= 'display: none' >Show the Show div</div>")
-  var body = $('body');
-  $(body).append(this.$show);
+  var footer = $('footer');
+  $(footer).before(this.$show);
 }
 
 
