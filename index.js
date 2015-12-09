7c5ad66b7fcaa74ca57e00094d948cf2a828c8b0
Elise Kain <elise.kain@gmail.com>
2015-12-09 13:31:43 -0500

    mongooseUri formatting

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ var usersController = require('./controllers/users');
 var uriUtil = require('mongodb-uri')
 var favicon = require('serve-favicon');
 
-// var mongodbUri = 'mongodb://heroku_fw2w8pxs:tesa7kp6vfnpq6tdec9pb99ktj@ds027155.mongolab.com:27155/heroku_fw2w8pxs';
-// var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+var mongodbUri = 'mongodb://heroku_fw2w8pxs:tesa7kp6vfnpq6tdec9pb99ktj@ds027155.mongolab.com:27155/heroku_fw2w8pxs';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 // mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect('mongodb://heroku_fw2w8pxs:tesa7kp6vfnpq6tdec9pb99ktj@ds027155.mongolab.com:27155/heroku_fw2w8pxs');//we will have a headache when we deploy to heroku
+mongoose.connect(mongooseUri);//we will have a headache when we deploy to heroku
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
