9df91430d8042b80b6c28a4a4c99f924eba12e62
Elise Kain <elise.kain@gmail.com>
2015-12-11 10:22:28 -0500

    Append frequencies div to header instead of before footer

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   var $freqs = $("<div class= 'frequencies'><button class='btn goHome'>Home</button><button class='btn newFrequencyButton'>New Frequency</button></div>")
 
-  var footer = $('footer');
-  $(footer).before($freqs);
+  // var footer = $('footer');
+  $('header').after($freqs);
 
   // Frequency.fetch().then(function(frequencies){
     listView = new FrequencyListView($freqs);
